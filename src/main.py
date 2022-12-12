080d95f591c6b96c7a48ac5af47ee7ea67eea309
James Scherer <jscherer@uab.edu>
2022-12-12 09:58:33 -0600

    added test coverage print out. Right now it should fail

--- src/main.py
+++ src/main.py
@@ -7,4 +7,4 @@ app = FastAPI()
 @app.get("/")
 async def root():
     """ This is our root function for FastAPI test application """
-    return {"message": "Hello World"}
+    return {"message": "Hello Worldd"}
